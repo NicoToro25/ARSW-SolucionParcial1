405aca81d09da5049eb2cf55ce82e81c5bf2a8ff
NicoToro25 <nikotoro25@gmail.com>
2025-09-19 22:32:02 -0500

    first commit

---
+++ src/test/java/edu/eci/arsw/Practica/PracticaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.Practica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
